a07ee3b88dd705642f66c2eecfd7ef7c2b129b78
shravanrn@gmail.com <shravanrn@gmail.com>
2020-05-20 01:07:21 -0700

    Make "spectre_only_sandbox_isolation" a setting

--- cranelift-spectre/src/runtime.rs
+++ cranelift-spectre/src/runtime.rs
@@ -12,16 +12,18 @@ pub fn perform_transition_protection_in() {
     }
     // Hack: In an actual implementation, this if condition is bad. You would want to do this unconditionally or with cmovs
     // But that's just engineering work
-    let mitigation = get_spectre_mitigation();
-    if mitigation == SpectreMitigation::LOADLFENCE
-        || mitigation == SpectreMitigation::STRAWMAN
-        || mitigation == SpectreMitigation::SFI
-        || mitigation == SpectreMitigation::CET
-    {
-        // also use cmovs in a real implementation
-        // core switch to sandbox
-        if mitigation == SpectreMitigation::SFI {
-            //BTB flush
+    let only_sandbox_isolation = get_spectre_only_sandbox_isolation();
+    if !only_sandbox_isolation {
+        let mitigation = get_spectre_mitigation();
+        if mitigation == SpectreMitigation::LOADLFENCE
+            || mitigation == SpectreMitigation::STRAWMAN
+            || mitigation == SpectreMitigation::SFI
+            || mitigation == SpectreMitigation::CET
+        {
+            // core switch to sandbox
+            if mitigation == SpectreMitigation::SFI {
+                //BTB flush
+            }
         }
     }
 }
--- cranelift-spectre/src/settings.rs
+++ cranelift-spectre/src/settings.rs
@@ -4,11 +4,13 @@ use num_derive::FromPrimitive;
 struct SpectreSettings {
     spectre_mitigation: SpectreMitigation,
     spectre_pht_mitigation: SpectrePHTMitigation,
+    spectre_only_sandbox_isolation: bool,
 }
 
 static mut SPECTRE_SETTINGS: SpectreSettings = SpectreSettings {
     spectre_mitigation: SpectreMitigation::NONE,
     spectre_pht_mitigation: SpectrePHTMitigation::NONE,
+    spectre_only_sandbox_isolation: false,
 };
 
 #[derive(PartialEq, Debug, Clone, Copy, FromPrimitive)]
@@ -26,9 +28,27 @@ pub enum SpectrePHTMitigation {
     BLADE,
 }
 
+pub fn get_default_pht_protection(
+    spectre_mitigation: Option<SpectreMitigation>,
+    spectre_only_sandbox_isolation: bool,
+) -> Option<SpectrePHTMitigation> {
+    if spectre_mitigation.is_none() || spectre_only_sandbox_isolation {
+        return Some(SpectrePHTMitigation::NONE);
+    }
+
+    let spectre_mitigation = spectre_mitigation.unwrap();
+    if spectre_mitigation == SpectreMitigation::SFI || spectre_mitigation == SpectreMitigation::CET
+    {
+        return Some(SpectrePHTMitigation::BLADE);
+    }
+
+    return Some(SpectrePHTMitigation::NONE);
+}
+
 pub fn use_spectre_mitigation_settings(
     spectre_mitigation: Option<SpectreMitigation>,
     spectre_pht_mitigation: Option<SpectrePHTMitigation>,
+    spectre_only_sandbox_isolation: bool,
 ) {
     let spectre_mitigation = spectre_mitigation.unwrap_or(get_spectre_mitigation());
     let spectre_pht_mitigation = spectre_pht_mitigation.unwrap_or(get_spectre_pht_mitigation());
@@ -37,6 +57,7 @@ pub fn use_spectre_mitigation_settings(
         SPECTRE_SETTINGS = SpectreSettings {
             spectre_mitigation,
             spectre_pht_mitigation,
+            spectre_only_sandbox_isolation,
         };
     }
 }
@@ -50,5 +71,11 @@ pub fn get_spectre_mitigation() -> SpectreMitigation {
 pub fn get_spectre_pht_mitigation() -> SpectrePHTMitigation {
     unsafe {
         return SPECTRE_SETTINGS.spectre_pht_mitigation.clone();
+    }
+}
+
+pub fn get_spectre_only_sandbox_isolation() -> bool {
+    unsafe {
+        return SPECTRE_SETTINGS.spectre_only_sandbox_isolation.clone();
     }
 }
